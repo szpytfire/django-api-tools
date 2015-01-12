d9f36422e24a5406e3dbc0f824abdd5cd22bc779
szpytfire <mail@tomszpytman.com>
2015-01-12 15:03:12 +0000

    Updated setup.py for PyPi

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ __author__ = 'szpytfire'
 setup(
   name = 'django_api_tools',
   packages = ['django_api_tools'],
-  version = '0.1',
+  version = '0.1.1',
   description = 'Django API add-on is a mini-framework which allows developers to run RESTful APIs alongside websites using Forms/Templates.',
   author = 'Tom Szpytman',
   author_email = 'mail@tomszpytman.com',
   url = 'https://github.com/szpytfire/django-api-tools',
-  download_url = 'https://github.com/szpytfire/django-api-tools/tarball/0.1',
+  download_url = 'https://github.com/szpytfire/django-api-tools/tarball/0.1.1',
   keywords = ['django', 'api', 'rest'],
   classifiers = [],
 )
